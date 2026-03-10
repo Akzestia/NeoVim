d2cc88cc450af7e8e82edf273e916e1b0962daea
アクゼスティア <akzestia@gmail.com>
2026-03-10 22:34:34 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,13 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 vim.opt.swapfile = false
+vim.opt.clipboard = "unnamedplus"
 
 require("config.lazy")
-
-vim.opt.clipboard = "unnamedplus"
 
 require("lspconfig").qmlls.setup({
   cmd = { "qmlls" },
   filetypes = { "qml", "qbs" },
   root_dir = require("lspconfig.util").root_pattern("CMakeLists.txt", ".git", ".qmlls.ini"),
   settings = {
-    -- This helps the server find the built-in Qt modules
     importPaths = { "/Users/akzestia/Qt/6.10.2/macos/qml" },
   },
 })

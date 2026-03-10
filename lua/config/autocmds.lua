1e63e1f96f41bd5f768b9e1d823c9d5bb8a4f3be
アクゼスティア <akzestia@gmail.com>
2026-03-10 22:35:45 +0900

    Update autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,15 +1,5 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    -- close explorer if it opened automatically
     for _, picker in ipairs(Snacks.picker.get({ source = "explorer" })) do
       picker:close()
     end
@@ -31,7 +21,6 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.opt_local.concealcursor = "n"
     vim.cmd([[syntax match envSecret /=\zs.\+/ conceal cchar=*]])
 
-    -- toggle with <leader>eh
     vim.keymap.set("n", "<leader>[[", function()
       vim.opt_local.conceallevel = vim.wo.conceallevel > 0 and 0 or 2
     end, { buffer = true, desc = "Toggle .env hiding" })
